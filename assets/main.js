9ce08e648dbafd2214b2853c6f28e97ce467b8b2
火火clover <hicug@qq.com>
2022-10-19 18:09:04 +0800

    update: 更新中文简历

--- assets/main.js
+++ assets/main.js
@@ -155,8 +155,12 @@ module.exports = ({lang}) => (lang === "cn" && {
         workExperience("2021/09-现在",
             "铁路运输调度命令管理系统全栈",
             "全栈开发",
-            `研究生期间参与的导师项目中，独自设计并全栈完成了此项目。出于跨平台和快速开发的考虑，
-            客户端采用electron框架编写，并尽可能将大部分功能尽可能在页面端完成开发`,
+            `研究生期间跟随导师完成此项目，独自设计并开发完成了此项目的全栈开发，
+            包括系统的客户端和服务端，以及调用铁路LKJ2000黑匣子系统的IC卡写卡设备的
+            跨平台驱动的开发，受该系统的开发时代以及疫情防控限制，在没有该设备的任何开
+            发文档和源代码的情况下解析出了该黑匣子系统的IC卡写卡设备的通信协议以及IC卡的存
+            储协议。并出于跨平台和快速开发的考虑，客户端采用国际上常用的开源跨平台框架
+            （例如electron，Cordova）编写，并尽可能将大部分功能尽可能在页面端完成开发`,
             ["使用electron框架编写客户端",
                 "客户端页面采用mvvm架构",
                 "使用typescript+react+mui5完成了页面的编写",
@@ -173,10 +177,11 @@ module.exports = ({lang}) => (lang === "cn" && {
                 "strapi",
                 "c++",
                 "libusb"],
-            ["完成读卡器写卡格式破解的demo页",
-                `完成整个系统的登录、调度命令创建、管理、审核、分解、写卡等功能的前端展示和后端接口
-                的编写`,
-                "完成了系统的跨Windows、Linux、Android三个平台的发布"]),
+            [`解析了读卡器的usb通信协议，完成了系统跨平台驱动的开发`,
+                `基于开发出的跨平台设备驱动，完成了IC卡存储协议的解析工具的开发，并成功完成了控车IC卡的解析`,
+                `完成整个系统的登录、调度命令创建、管理、审核、分解、写卡等功能的前端展示和后端接口的编写`,
+                `通过了LKJ2000的实机读卡测试，使用本系统写出的IC卡控车命令可以控制列车的运行`,
+                `完成了系统的跨Windows、Linux、Android三个平台的发布`]),
         workExperience("2022/06 - 2022/09",
             "前端实习",
             "前端实习培训生",
@@ -192,12 +197,14 @@ module.exports = ({lang}) => (lang === "cn" && {
             ["css+html+js", "ts", "es6", "webpack", "react", "nodejs"],
             ["完成了十余项课后作业任务，包括css训练、响应式布局训练、es6训练、typescript训练、webpack训练、react训练等",
                 "完成了中期考核任务，本人负责解决客户端的复杂逻辑，以及管理器端的商品和订单管理，管理商品和订单项目，完成任务的同时并帮助团队成员解决项目中的问题",
-                "完成了终期考核任务，完成了登录、注册和用户信息设置等全部的用户管理，并完成了七种类型问题的表单创建、编辑、预览、管理、填充、查看、表单二维码共享等功能"]),
+                "完成了终期考核任务，完成了登录、注册和用户信息设置等全部的用户管理，并完成了七种类型问题的表单创建、编辑、预览、管理、填充、查看、表单二维码共享等功能",
+                "通过了金山的训练营培训，顺利拿到了培训营结业证书"]),
         workExperience("2020/11-2021/03",
             "地质灾害信息管理系统",
             "前端开发",
-            `研究生期间参与本项目，完成地质灾害信息的增删查改，以及其他复杂的逻辑，使用到了
-            ruoyi快速构建，neo4j图数据库，直播，文本识别等技术`,
+            `研究生期间参与本项目，完成地质灾害信息的增删查改，以及其他复杂的逻辑的开发，使用
+            到了ruoyi快速构建，neo4j图数据库，直播，文本识别等技术，目前该项目的软著已发布，依托
+            该项目已发表出两篇论文`,
             ["使用ruoyi完成基础功能框架的搭建",
                 "使用vue2+elementUI完成CRUD基础页面的开发",
                 "完成除CRUD外，雨量计算、灾害等级计算、数据统计、neo4j可视化、直播等功能"],
@@ -207,19 +214,20 @@ module.exports = ({lang}) => (lang === "cn" && {
                 "使用react完成了雨量计算和灾害等级计算组件的开发",
                 "使用mpegts.js完成了直播功能的开发",
                 "使用echarts完成了数据统计表展示的功能"]),
+        workExperience("2020/09-2022/11",
+            "铁路LKJ2000黑匣子系统IC卡写卡设备驱动Linux移植",
+            "设备通信协议解析和驱动开发",
+            `研究生期间参与本项目，受该LKJ2000的开发时代以及疫情防控限制，该项目没有任何的开
+            发文档和源码资料，南宁铁路局方仅提供了该系统完整的IC卡和读卡器设备，以及全套的软件和使用
+            环境。后对驱动以及软件和dll调用库等进行了反汇编并debug，根据读卡器的芯片特性，解析出关
+            键数据，以及关键usb ep地址和通讯协议，并使用开源库libusb，完成了设备驱动的二次开发，并
+            使其具有了跨平台的能力，为后续系统的跨平台开发提供了关键基础。`),
         workExperience("2021/09-2022/03",
             "工程安全质量考核app",
             "安卓端开发",
             `研究生期间参与本项目的开发，负责安卓前端的开发工作，实现了用户登录，工程项目打分，
             打分表修改、打印、下载等模块，工程打分提供了简易模式以及传统手写模式的填写，打印表提供了
-            pdf预览、手写签字等功能（app下载地址）`),
-        workExperience("2020/09-2022/11",
-            "铁路运行揭示调度命令读卡驱动Linux移植",
-            "驱动破解和开发",
-            `研究生期间参与本项目，受铁路局项目机密性的限制，只有usb读卡器的驱动程序、软件程序，
-            后对驱动以及软件和dll调用库等进行了反汇编和dubug，根据读卡器的芯片特性，破解出关键数据，
-            以及关键ep地址和通讯协议，完成了调用库Linux的移植，并具有跨平台的能力，代码在原windows
-            平台上依然可编译运行`),
+            pdf预览、pdf手写标注、手写签字等功能`),
         workExperience("2020/09-2020/12",
             "Python深度学习目标检测和字符识别",
             "模型代码编写",
@@ -231,8 +239,21 @@ module.exports = ({lang}) => (lang === "cn" && {
             "其他",
             "个人业余开发",
             `课余时间自主学习了多种语言及框架，并帮助他人或自己使用开发了开发了多个脚本工具，
-            爬虫，网站，接口，群聊机器人等，开发的网站包括校园问答百科（链接），社团值班排班工具
-            （链接），校园新闻（链接），校园拼车平台（已下线）`),
+            爬虫，网站，接口，群聊机器人等，开发的网站包括校园问答百科，社团值班排班工具，校园新
+            闻，校园拼车平台（已下线）`),
+    ]),
+    Campus_Organization_Experience: "社团组织经历",
+    otherExperiences: workExperiences([
+        workExperience("2020/09 - 2023/06",
+            "班委",
+            "班级团支书",
+            `在班级中组织过多次讲党史、十九大、防诈骗等主题的团支部会议；带领班级的思想建设工作；
+            为班级的入党推优工作严格把关`),
+        workExperience("2016/09 - 2022/06",
+            "其他", "社团和活动",
+            `在校期间参与过多个社团以及活动，担任过吉他协会公关部部长，创业基地公关部副部长，
+            创业中心新媒体主席，参加过校民乐团等，后皆因考研的缘故退出；创建了一个校内的自媒体平台，
+            带领一支20人左右的运营团队，换届转手前已有超过一万的粉丝数`)
     ])
 }) || (lang === "en" && {
     NAME: "张楚研",
@@ -370,24 +391,70 @@ module.exports = ({lang}) => (lang === "cn" && {
             ["vue2", "elementUI", "neo4j", "neovis", "mpegts", "echarts", "react"],
             ["Finished the basic CRUD page writing",
                 "Use react + neovis to complete the development of neo4j visual plug-in",
-                "使用react完成了雨量计算和灾害等级计算组件的开发",
-                "使用mpegts.js完成了直播功能的开发",
-                "使用echarts完成了数据统计表展示的功能"]),
+                "Use react + mui5 to complete the development of Rainfall Calculation and disaster grade calculation components",
+                "Use mpegts.js to complete the development of live broadcast function",
+                "Use echarts to complete the function of displaying the data statistics table"]),
         workExperience("2021/09-2022/03",
-            "工程安全质量考核app",
-            "安卓端开发",
-            "研究生期间参与本项目的开发，负责安卓前端的开发工作，实现了用户登录，工程项目打分，打分表修改、打印、下载等模块，工程打分提供了简易模式以及传统手写模式的填写，打印表提供了pdf预览、手写签字等功能（app下载地址）"),
+            "Project safety and quality assessment app",
+            "android develop",
+            `I participated in the development of this project during 
+            my postgraduate study and was responsible for the development 
+            of the Android front-end. I realized modules such as user login, 
+            project scoring, modification, printing and downloading of the 
+            scoring table. The project scoring provides simple mode and 
+            traditional handwriting mode. The printing table provides PDF 
+            preview, handwriting signature and other functions`),
         workExperience("2020/09-2022/11",
-            "铁路运行揭示调度命令读卡驱动Linux移植",
-            "驱动破解和开发",
-            "研究生期间参与本项目，受铁路局项目机密性的限制，只有usb读卡器的驱动程序、软件程序，后对驱动以及软件和dll调用库等进行了反汇编和dubug，根据读卡器的芯片特性，破解出关键数据，以及关键ep地址和通讯协议，完成了调用库Linux的移植，并具有跨平台的能力，代码在原windows平台上依然可编译运行"),
+            "Linux porting of card reading driver",
+            "usb driver cracking and develop",
+            `I participated in this project during my postgraduate study. 
+            Restricted by the confidentiality of the railway bureau project, 
+            I only had the driver and software program of the USB card reader. 
+            After that, I disassembled and debugged the driver, software and 
+            DLL call library. According to the chip characteristics of the card 
+            reader, I cracked the key data, as well as the key EP address and 
+            communication protocol, completed the migration of the call library 
+            Linux, and had the ability of cross platform, The code can still be 
+            compiled and run on the original windows platform`),
         workExperience("2020/09-2020/12",
-            "Python深度学习目标检测和字符识别",
-            "模型代码编写",
-            "在研究生学习期间，独立完成了此课题。主题是识别火车车厢上打印的数字，数字不能被普通OCR识别，上一个解决方案是使用一个模型来跟踪角色的位置，以排除无效信息，并使用另一个模型直接读取文本，在我的方案中视数字为logo，并训练一个YOLO模型来直接跟踪这些logo，并对其进行排序，一个模型使用的时间比两个要短得多"),
+            "Python deep-learning object detective and character recognization",
+            "coding of the model",
+            `Finished this topic  by myself during my postgraduate study. The 
+            topic was recognizing the number printed on train cars, the nums can’t 
+            be recognized by general OCR, the prev solution was use a model to track 
+            the character’s position to exclude the invalid info and use another 
+            read the text directly, I treated the num as logo and trained a YOLO 
+            model to directly track these logos and sort them, the time one model 
+            uses was much shorter than two.`),
         workExperience("2019/06-2023/09",
-            "其他",
-            "个人业余开发",
-            "课余时间自主学习了多种语言及框架，并帮助他人或自己使用开发了开发了多个脚本工具，爬虫，网站，接口，群聊机器人等，开发的网站包括校园问答百科（链接），社团值班排班工具（链接），校园新闻（链接），校园拼车平台（已下线）"),
+            "Others",
+            "developments in my spare time",
+            `In the spare time, I learned a variety of languages and frameworks 
+            independently, and helped others or myself to use them. I developed a 
+            number of script tools, spiders, websites, interfaces, group chat robots, 
+            etc. the developed websites include the campus question and answer 
+            Encyclopedia, the community duty scheduling tool, the campus 
+            news, and the campus carpool platform (offline)`),
+    ]),
+    Campus_Organization_Experience: "Campus Organization Experience",
+    otherExperiences: workExperiences([
+        workExperience("2020/09 - 2023/06",
+            "Class committee",
+            "Class League Secretary",
+            `In the class, I had organized many League branch meetings on the 
+            topics of party history, the 19th national congress, and fraud prevention;
+            Lead the ideological construction of the class; Strictly control the work 
+            of promoting excellence for the class to join the Party`),
+        workExperience("2016/09 - 2022/06",
+            "Associations", "Campus Community activities",
+            `Participated in many associations and activities during the 
+            school period. Has served as the director of the public relations 
+            department of the guitar association, the deputy director of the 
+            public relations department of the entrepreneurship base, the chairman 
+            of the new media of the entrepreneurship center, and participated in 
+            the school folk orchestra. Created an internal we media platform, 
+            led an operation team of about 20 members, and had more than 10000 
+            fans before changing hands. All withdrew because of the postgraduate 
+            entrance examination after that`)
     ])
 })
